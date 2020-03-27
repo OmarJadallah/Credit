9387d2d8fcec90f9fa28e2f8356dab533ea385ef
OmarJadallah <60223235+OmarJadallah@users.noreply.github.com>
2020-03-27 14:41:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,7 +213,7 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-abc')){
-if (message.author.id !== '263639609416613888') return message.reply('** You need to be a donator to do this coand   **')
+if (message.author.id !== '690510385526079568') return message.reply('** You need to be a donator to do this coand   **')
 message.channel.sendMessage('Cool Stuff are happening    |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
